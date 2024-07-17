91c5f5f84e3f68ccb17de897352869a5d4e8bf23
Brandon Henricks <78803523+bluemodus-brandon@users.noreply.github.com>
2024-07-17 19:28:57 -0400

    Enhance data context and DI setup
    This commit introduces the IXperienceDataContext interface and its concrete implementation, XperienceDataContext, to centralize the data context in the Xperience by Kentico CMS application. It includes methods for accessing content item, page content, and reusable schema contexts. Additionally, the DependencyInjection.cs file has been updated to register IXperienceDataContext as a scoped service, improving the dependency injection setup. Interface adjustments include the introduction of a marker interface, IProcessor, and an update to IProcessor<T> to inherit from it, enhancing the processor type system. Code organization has been improved with new using directives for better readability and maintainability. These changes collectively improve the application's architecture, making it more organized, modular, and extensible.

---
+++ src/XperienceCommunity.DataContext/Interfaces/IXperienceDataContext.cs
@@ -0,0 +1,32 @@
+﻿using CMS.ContentEngine;
+using CMS.Websites;
+
+namespace XperienceCommunity.DataContext.Interfaces
+{
+    /// <summary>
+    /// Represents the data context for Xperience by Kentico CMS.
+    /// </summary>
+    public interface IXperienceDataContext
+    {
+        /// <summary>
+        /// Gets the content item context for the specified content type.
+        /// </summary>
+        /// <typeparam name="T">The type of the content item.</typeparam>
+        /// <returns>The content item context.</returns>
+        IContentItemContext<T> ForContentType<T>() where T : class, IContentItemFieldsSource, new();
+
+        /// <summary>
+        /// Gets the page content context for the specified page content type.
+        /// </summary>
+        /// <typeparam name="T">The type of the page content.</typeparam>
+        /// <returns>The page content context.</returns>
+        IPageContentContext<T> ForPageContentType<T>() where T : class, IWebPageFieldsSource, new();
+
+        /// <summary>
+        /// Gets the reusable schema context for the specified schema.
+        /// </summary>
+        /// <typeparam name="T">The type of the schema.</typeparam>
+        /// <returns>The reusable schema context.</returns>
+        IReusableSchemaContext<T> ForReusableSchema<T>();
+    }
+}
---
+++ src/XperienceCommunity.DataContext/XperienceDataContext.cs
@@ -0,0 +1,57 @@
+﻿using CMS.ContentEngine;
+using CMS.Helpers;
+using CMS.Websites;
+using CMS.Websites.Routing;
+using Microsoft.Extensions.Logging;
+using XperienceCommunity.DataContext.Configurations;
+using XperienceCommunity.DataContext.Interfaces;
+
+namespace XperienceCommunity.DataContext;
+
+public sealed class XperienceDataContext : IXperienceDataContext
+{
+    private readonly IProgressiveCache _cache;
+    private readonly XperienceDataContextConfig _config;
+    private readonly IContentQueryExecutor _executor;
+    private readonly ILoggerFactory _loggerFactory;
+    private readonly IEnumerable<IProcessor> _processors;
+    private readonly IWebsiteChannelContext _websiteChannelContext;
+
+    public XperienceDataContext(IProgressiveCache cache, IWebsiteChannelContext websiteChannelContext,
+        IEnumerable<IProcessor> processors, XperienceDataContextConfig config, IContentQueryExecutor executor,
+        ILoggerFactory loggerFactory)
+    {
+        _cache = cache;
+        _websiteChannelContext = websiteChannelContext;
+        _processors = processors;
+        _config = config;
+        _executor = executor;
+        _loggerFactory = loggerFactory;
+    }
+
+    public IContentItemContext<T> ForContentType<T>() where T : class, IContentItemFieldsSource, new()
+    {
+        var logger = _loggerFactory.CreateLogger<ContentQueryExecutor<T>>();
+
+        var executor = new ContentQueryExecutor<T>(logger, _executor, _processors.OfType<IContentItemProcessor<T>>());
+
+        return new ContentItemContext<T>(_websiteChannelContext, _cache, executor, _config);
+    }
+
+    public IPageContentContext<T> ForPageContentType<T>() where T : class, IWebPageFieldsSource, new()
+    {
+        var logger = _loggerFactory.CreateLogger<PageContentQueryExecutor<T>>();
+        var executor =
+            new PageContentQueryExecutor<T>(logger, _executor, _processors.OfType<IPageContentProcessor<T>>());
+        return new PageContentContext<T>(_cache, executor, _websiteChannelContext, _config);
+    }
+
+    public IReusableSchemaContext<T> ForReusableSchema<T>()
+    {
+        var logger = _loggerFactory.CreateLogger<ReusableSchemaExecutor<T>>();
+
+        var executor = new ReusableSchemaExecutor<T>(logger, _executor);
+
+        return new ReusableSchemaContext<T>(_websiteChannelContext, _cache, executor, _config);
+    }
+}
