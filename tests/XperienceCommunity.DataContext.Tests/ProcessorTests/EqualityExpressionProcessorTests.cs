7642fcb4fc06ae734769671c37471d12532e69e1
Brandon Henricks <brandon.henricks1984@gmail.com>
2025-07-10 08:11:17 -0400

    Add unit tests for EqualityExpressionProcessor
    This commit introduces a suite of unit tests for the `EqualityExpressionProcessor` class in the `XperienceCommunity.DataContext` namespace. The tests cover various scenarios for processing equality expressions, including member-constant comparisons, unary expressions, and invalid expressions. NSubstitute is used to create mock contexts and verify method calls with expected parameters. Additionally, a theory-based test is included to validate the processor's handling of different expression type combinations.

--- tests/XperienceCommunity.DataContext.Tests/ProcessorTests/EqualityExpressionProcessorTests.cs
+++ tests/XperienceCommunity.DataContext.Tests/ProcessorTests/EqualityExpressionProcessorTests.cs
@@ -1,4 +1,6 @@
-﻿using NSubstitute;
+﻿using System.Linq.Expressions;
+using CMS.ContentEngine;
+using NSubstitute;
 using XperienceCommunity.DataContext.Interfaces;
 using Xunit;
 
@@ -15,4 +17,154 @@ public class EqualityExpressionProcessorTests
 
         Assert.NotNull(processor);
     }
+    [Fact]
+    public void Process_MemberEqualsConstant_AddsParameterAndWhereEquals()
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        var member = Expression.Property(Expression.Parameter(typeof(string), "x"), "Length");
+        var constant = Expression.Constant(5);
+        var binary = Expression.Equal(member, constant);
+
+        processor.Process((BinaryExpression)binary);
+
+        context.Received().AddParameter("Length", 5);
+        context.Received().AddWhereAction(Arg.Any<Action<WhereParameters>>());
+    }
+
+    [Fact]
+    public void Process_ConstantEqualsMember_AddsParameterAndWhereEquals()
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        var member = Expression.Property(Expression.Parameter(typeof(string), "x"), "Length");
+        var constant = Expression.Constant(5);
+        var binary = Expression.Equal(constant, member);
+
+        processor.Process((BinaryExpression)binary);
+
+        context.Received().AddParameter("Length", 5);
+        context.Received().AddWhereAction(Arg.Any<Action<WhereParameters>>());
+    }
+
+    [Fact]
+    public void Process_MemberEqualsUnaryConstant_AddsParameterAndWhereEquals()
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        var member = Expression.Property(Expression.Parameter(typeof(string), "x"), "Length");
+        var constant = Expression.Constant(5);
+        var unary = Expression.Convert(constant, typeof(int));
+        var binary = Expression.Equal(member, unary);
+
+        processor.Process((BinaryExpression)binary);
+
+        context.Received().AddParameter("Length", 5);
+        context.Received().AddWhereAction(Arg.Any<Action<WhereParameters>>());
+    }
+
+    [Fact]
+    public void Process_UnaryConstantEqualsMember_AddsParameterAndWhereEquals()
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        var member = Expression.Property(Expression.Parameter(typeof(string), "x"), "Length");
+        var constant = Expression.Constant(5);
+        var unary = Expression.Convert(constant, typeof(int));
+        var binary = Expression.Equal(unary, member);
+
+        processor.Process((BinaryExpression)binary);
+
+        context.Received().AddParameter("Length", 5);
+        context.Received().AddWhereAction(Arg.Any<Action<WhereParameters>>());
+    }
+
+    [Fact]
+    public void Process_MemberEqualsMember_AddsParameterAndWhereEquals()
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        var param = Expression.Constant("hello");
+        var leftMember = Expression.Property(param, "Length");
+        var rightMember = Expression.Property(Expression.Constant("world!"), "Length");
+        var binary = Expression.Equal(leftMember, rightMember);
+
+        processor.Process((BinaryExpression)binary);
+
+        context.Received().AddParameter("Length", 6);
+        context.Received().AddWhereAction(Arg.Any<Action<WhereParameters>>());
+    }
+
+    [Fact]
+    public void Process_ConstantEqualsConstant_AddsWhereEqualsWithResult()
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        var left = Expression.Constant(5);
+        var right = Expression.Constant(5);
+        var binary = Expression.Equal(left, right);
+
+        processor.Process((BinaryExpression)binary);
+
+        context.Received().AddWhereAction(Arg.Any<Action<WhereParameters>>());
+    }
+
+    [Fact]
+    public void Process_InvalidExpression_ThrowsInvalidExpressionFormatException()
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        var left = Expression.Parameter(typeof(int), "x");
+        var right = Expression.Parameter(typeof(int), "y");
+        var binary = Expression.Add(left, right);
+
+        Assert.Throws<XperienceCommunity.DataContext.Exceptions.InvalidExpressionFormatException>(() =>
+            processor.Process((BinaryExpression)binary));
+    }
+
+    [Theory]
+    [InlineData(typeof(MemberExpression), typeof(ConstantExpression), true)]
+    [InlineData(typeof(ConstantExpression), typeof(MemberExpression), true)]
+    [InlineData(typeof(MemberExpression), typeof(UnaryExpression), true)]
+    [InlineData(typeof(UnaryExpression), typeof(MemberExpression), true)]
+    [InlineData(typeof(MemberExpression), typeof(MemberExpression), true)]
+    [InlineData(typeof(ConstantExpression), typeof(ConstantExpression), false)]
+    [InlineData(typeof(ConstantExpression), typeof(ParameterExpression), false)]
+    public void CanProcess_ReturnsExpectedResult(Type leftType, Type rightType, bool expected)
+    {
+        var context = Substitute.For<IExpressionContext>();
+        var processor = new Processors.EqualityExpressionProcessor(context);
+
+        Expression left = leftType switch
+        {
+            Type t when t == typeof(ConstantExpression) => Expression.Constant(1),
+            Type t when t == typeof(MemberExpression) => Expression.Property(Expression.Parameter(typeof(string), "x"), "Length"),
+            Type t when t == typeof(UnaryExpression) => Expression.Convert(Expression.Constant(1), typeof(int)),
+            Type t when t == typeof(ParameterExpression) => Expression.Parameter(typeof(int), "x"),
+            _ => throw new NotSupportedException($"Type {leftType} not supported")
+        };
+
+        Expression right = rightType switch
+        {
+            Type t when t == typeof(ConstantExpression) => Expression.Constant(2),
+            Type t when t == typeof(MemberExpression) => Expression.Property(Expression.Parameter(typeof(string), "y"), "Length"),
+            Type t when t == typeof(UnaryExpression) => Expression.Convert(Expression.Constant(2), typeof(int)),
+            Type t when t == typeof(ParameterExpression) => Expression.Parameter(typeof(int), "y"),
+            _ => throw new NotSupportedException($"Type {rightType} not supported")
+        };
+
+        var binary = Expression.MakeBinary(ExpressionType.Equal, left, right);
+
+        var result = processor.CanProcess(binary);
+
+        Assert.Equal(expected, result);
+    }
+
 }
