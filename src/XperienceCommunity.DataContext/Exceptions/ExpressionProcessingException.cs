da12ef94c8212771f4b195b33f80d05b87abab13
Brandon Henricks <brandon.henricks1984@gmail.com>
2025-07-09 20:57:04 -0400

    Add logo and README to project packaging
    Updated `Directory.Build.props` to include `logo.png` and `README.md` in the project packaging. The logo is set to be not visible, while the README is included without specific visibility settings. A new binary file, `logo.png`, was also added.

---
+++ src/XperienceCommunity.DataContext/Exceptions/ExpressionProcessingException.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq.Expressions;
+
+namespace XperienceCommunity.DataContext.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when an expression cannot be processed.
+    /// </summary>
+    public class ExpressionProcessingException : Exception
+    {
+        /// <summary>
+        /// Gets the expression that caused the error.
+        /// </summary>
+        public Expression? Expression { get; }
+
+        /// <summary>
+        /// Gets the expression type name.
+        /// </summary>
+        public string? ExpressionTypeName { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionProcessingException"/> class.
+        /// </summary>
+        public ExpressionProcessingException() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionProcessingException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public ExpressionProcessingException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionProcessingException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="expression">The expression that caused the error.</param>
+        public ExpressionProcessingException(string message, Expression expression) : base(message)
+        {
+            Expression = expression;
+            ExpressionTypeName = expression.GetType().Name;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionProcessingException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public ExpressionProcessingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExpressionProcessingException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="expression">The expression that caused the error.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public ExpressionProcessingException(string message, Expression expression, Exception innerException)
+            : base(message, innerException)
+        {
+            Expression = expression;
+            ExpressionTypeName = expression.GetType().Name;
+        }
+    }
+}
---
+++ src/XperienceCommunity.DataContext/Exceptions/UnsupportedExpressionException.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+
+namespace XperienceCommunity.DataContext.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when an expression type is not supported.
+    /// </summary>
+    public sealed class UnsupportedExpressionException : ExpressionProcessingException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnsupportedExpressionException"/> class.
+        /// </summary>
+        /// <param name="expressionType">The unsupported expression type.</param>
+        public UnsupportedExpressionException(Type expressionType) 
+            : base($"The expression type '{expressionType.Name}' is not supported.")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnsupportedExpressionException"/> class.
+        /// </summary>
+        /// <param name="expressionType">The unsupported expression type.</param>
+        /// <param name="expression">The expression that caused the error.</param>
+        public UnsupportedExpressionException(ExpressionType expressionType, Expression expression) 
+            : base($"The expression type '{expressionType}' is not supported.", expression)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnsupportedExpressionException"/> class.
+        /// </summary>
+        /// <param name="methodName">The unsupported method name.</param>
+        /// <param name="expression">The expression that caused the error.</param>
+        public UnsupportedExpressionException(string methodName, Expression expression) 
+            : base($"The method '{methodName}' is not supported.", expression)
+        {
+        }
+
+        public UnsupportedExpressionException() : base()
+        {
+        }
+
+        public UnsupportedExpressionException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedExpressionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public UnsupportedExpressionException(string message, Expression expression, Exception innerException) : base(message, expression, innerException)
+        {
+        }
+    }
+}
