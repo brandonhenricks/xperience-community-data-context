4bb1ac4cb9e5e57fa4c29ee8aa65268904e0cb4a
Brandon Henricks <brandon.henricks1984@gmail.com>
2025-07-09 22:30:28 -0400

    Enhance documentation and refactor expression handling
    - Added XML documentation comments across multiple files for better clarity and maintainability.
    - Streamlined class structures in `XperienceDataContextConfig.cs`, `ExpressionProcessingException.cs`, `UnsupportedExpressionException.cs`, `ExpressionExtensions.cs`, and `TypeExtensions.cs`.
    - Removed unnecessary class declarations and redundant code to improve readability and efficiency.

--- src/XperienceCommunity.DataContext/Extensions/TypeExtensions.cs
+++ src/XperienceCommunity.DataContext/Extensions/TypeExtensions.cs
@@ -2,77 +2,76 @@
 using CMS.Websites;
 using System.Collections.Concurrent;
 
-namespace XperienceCommunity.DataContext.Extensions
+namespace XperienceCommunity.DataContext.Extensions;
+
+/// <summary>
+/// Provides extension methods for working with types.
+/// </summary>
+internal static class TypeExtensions
 {
+    private const string FieldName = "CONTENT_TYPE_NAME";
+
+    private const string ReusableSchemaFieldName = "REUSABLE_FIELD_SCHEMA_NAME";
+
+    private static readonly ConcurrentDictionary<string, string> s_classNames =
+        new ConcurrentDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
     /// <summary>
-    /// Provides extension methods for working with types.
+    /// Determines whether the specified type inherits from <see cref="IWebPageFieldsSource"/>.
     /// </summary>
-    internal static class TypeExtensions
+    /// <param name="type">The type to check.</param>
+    /// <returns><c>true</c> if the specified type inherits from <see cref="IWebPageFieldsSource"/>; otherwise, <c>false</c>.</returns>
+    internal static bool InheritsFromIWebPageFieldsSource(this Type type)
     {
-        private const string FieldName = "CONTENT_TYPE_NAME";
+        return typeof(IWebPageFieldsSource).IsAssignableFrom(type);
+    }
 
-        private const string ReusableSchemaFieldName = "REUSABLE_FIELD_SCHEMA_NAME";
+    /// <summary>
+    /// Determines whether the specified type inherits from <see cref="IContentItemFieldsSource"/>.
+    /// </summary>
+    /// <param name="type">The type to check.</param>
+    /// <returns><c>true</c> if the specified type inherits from <see cref="IContentItemFieldsSource"/>; otherwise, <c>false</c>.</returns>
+    internal static bool InheritsFromIContentItemFieldsSource(this Type type)
+    {
+        return typeof(IContentItemFieldsSource).IsAssignableFrom(type);
+    }
 
-        private static readonly ConcurrentDictionary<string, string> s_classNames =
-            new ConcurrentDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-
-        /// <summary>
-        /// Determines whether the specified type inherits from <see cref="IWebPageFieldsSource"/>.
-        /// </summary>
-        /// <param name="type">The type to check.</param>
-        /// <returns><c>true</c> if the specified type inherits from <see cref="IWebPageFieldsSource"/>; otherwise, <c>false</c>.</returns>
-        internal static bool InheritsFromIWebPageFieldsSource(this Type type)
+    /// <summary>
+    /// Gets the content type name associated with the specified type.
+    /// </summary>
+    /// <param name="type">The type to get the content type name for.</param>
+    /// <returns>The content type name associated with the specified type, or <c>null</c> if the type does not inherit from <see cref="IWebPageFieldsSource"/> or <see cref="IContentItemFieldsSource"/>, or if the content type name is not found.</returns>
+    internal static string? GetContentTypeName(this Type? type)
+    {
+        if (type is null)
         {
-            return typeof(IWebPageFieldsSource).IsAssignableFrom(type);
+            return null;
         }
 
-        /// <summary>
-        /// Determines whether the specified type inherits from <see cref="IContentItemFieldsSource"/>.
-        /// </summary>
-        /// <param name="type">The type to check.</param>
-        /// <returns><c>true</c> if the specified type inherits from <see cref="IContentItemFieldsSource"/>; otherwise, <c>false</c>.</returns>
-        internal static bool InheritsFromIContentItemFieldsSource(this Type type)
+        if (!type.InheritsFromIWebPageFieldsSource() && !type.InheritsFromIContentItemFieldsSource())
         {
-            return typeof(IContentItemFieldsSource).IsAssignableFrom(type);
+            return null;
         }
 
-        /// <summary>
-        /// Gets the content type name associated with the specified type.
-        /// </summary>
-        /// <param name="type">The type to get the content type name for.</param>
-        /// <returns>The content type name associated with the specified type, or <c>null</c> if the type does not inherit from <see cref="IWebPageFieldsSource"/> or <see cref="IContentItemFieldsSource"/>, or if the content type name is not found.</returns>
-        internal static string? GetContentTypeName(this Type? type)
+        if (string.IsNullOrWhiteSpace(type.FullName))
         {
-            if (type is null)
-            {
-                return null;
-            }
+            return null;
+        }
 
-            if (!type.InheritsFromIWebPageFieldsSource() && !type.InheritsFromIContentItemFieldsSource())
-            {
-                return null;
-            }
-
-            if (string.IsNullOrWhiteSpace(type.FullName))
-            {
-                return null;
-            }
-
-            if (s_classNames.TryGetValue(type.FullName, out var contentTypeName))
-            {
-                return contentTypeName;
-            }
-
-            contentTypeName = type.GetField(FieldName)?.GetRawConstantValue() as string;
-
-            if (string.IsNullOrWhiteSpace(contentTypeName))
-            {
-                return null;
-            }
-
-            s_classNames.TryAdd(type!.FullName, contentTypeName);
-
+        if (s_classNames.TryGetValue(type.FullName, out var contentTypeName))
+        {
             return contentTypeName;
         }
+
+        contentTypeName = type.GetField(FieldName)?.GetRawConstantValue() as string;
+
+        if (string.IsNullOrWhiteSpace(contentTypeName))
+        {
+            return null;
+        }
+
+        s_classNames.TryAdd(type!.FullName, contentTypeName);
+
+        return contentTypeName;
     }
 }
