0491fd359a5be2a7934a30b26bf76e55e63b80dc
Brandon Henricks <brandon.henricks1984@gmail.com>
2025-07-18 11:56:15 -0400

    Refactor ReusableSchemaExecutor and add unit tests
    Changed ReusableSchemaExecutor<T> to a non-sealed class to allow inheritance and updated the constructor to include logging. Removed unused static import in ContentQueryExecutorTests.cs. Expanded ReusableSchemaExecutorTests.cs with a new test class and added unit tests to validate constructor behavior.

---
+++ tests/XperienceCommunity.DataContext.Tests/ReusableSchemaExecutorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using CMS.ContentEngine;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+using XperienceCommunity.DataContext.Executors;
+
+namespace XperienceCommunity.DataContext.Tests.Executors
+{
+    public class ReusableSchemaExecutorTests
+    {
+        [Fact]
+        public void Constructor_ShouldThrow_IfLoggerIsNull()
+        {
+            var queryExecutor = Substitute.For<IContentQueryExecutor>();
+            Assert.Throws<ArgumentNullException>(() =>
+                new ReusableSchemaExecutor<object>(null!, queryExecutor));
+        }
+
+        [Fact]
+        public void Constructor_ShouldInstantiate()
+        {
+            var logger = Substitute.For<ILogger<ReusableSchemaExecutor<object>>>();
+            var queryExecutor = Substitute.For<IContentQueryExecutor>();
+            var executor = new ReusableSchemaExecutor<object>(logger, queryExecutor);
+            Assert.NotNull(executor);
+        }
+    }
+}
